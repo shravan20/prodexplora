33b8e2d752ed0d4765925b730efddc126ad22bea
shravan20 <mrshravankumarb@gmail.com>
2024-01-20 22:13:13 +0530

    setup: SSR monorepo structure setup

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,10 +1,36 @@
-npm run build
-echo "Build is complete"
+#!/usr/bin/env sh
+if [ -z "$husky_skip_init" ]; then
+  debug () {
+    if [ "$HUSKY_DEBUG" = "1" ]; then
+      echo "husky (debug) - $1"
+    fi
+  }
 
-cd apps/ui/ && 
-npm run lint
-echo "Linter validation complete"
+  readonly hook_name="$(basename -- "$0")"
+  debug "starting $hook_name..."
 
-cd ../ && cd ui/ && 
-npm run lint
-echo "Linter validation complete"
+  if [ "$HUSKY" = "0" ]; then
+    debug "HUSKY env variable is set to 0, skipping hook"
+    exit 0
+  fi
+
+  if [ -f ~/.huskyrc ]; then
+    debug "sourcing ~/.huskyrc"
+    . ~/.huskyrc
+  fi
+
+  readonly husky_skip_init=1
+  export husky_skip_init
+  sh -e "$0" "$@"
+  exitCode="$?"
+
+  if [ $exitCode != 0 ]; then
+    echo "husky - $hook_name hook exited with code $exitCode (error)"
+  fi
+
+  if [ $exitCode = 127 ]; then
+    echo "husky - command not found in PATH=$PATH"
+  fi
+
+  exit $exitCode
+fi
