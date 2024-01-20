07e3e35c93bada8d8f30e94210ae26573f6e96c6
shravan20 <mrshravankumarb@gmail.com>
2024-01-21 04:29:35 +0530

    chore: add new linter

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,14 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: join("../../../", '.env')
+        }),
         ServeStaticModule.forRoot({
+            /**
+             * TOOO: Make this configurable
+             */
             rootPath: join(__dirname, '../..', 'ui', 'dist'),
         }),
     ],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.setGlobalPrefix('/api');
+    app.setGlobalPrefix("/api");
     await app.listen(3000);
-    console.log('Server running on:' + 3000);
+    console.log("Server running on:", process.env.SERVER_PORT || 3000);
 }
 bootstrap();
