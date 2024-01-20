374b9c89c4634f8e55790847820bd3ef2f54ffd0
S K B <47430686+shravan20@users.noreply.github.com>
2024-01-21 00:29:22 +0530

    chore: Fix issue with precommit hook (#14)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,9 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix("/api");
-  await app.listen(3000);
-  console.log("Server running on:" + 3000)
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('/api');
+    await app.listen(3000);
+    console.log('Server running on:' + 3000);
 }
 bootstrap();
