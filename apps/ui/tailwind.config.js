f84e074d43ce9ebe52c6178e9486957abbbbeac3
ArnavNath550 <130316679+ArnavNath550@users.noreply.github.com>
2024-01-21 03:23:43 +1300

    Styles: Tailwind (#13)

---
+++ apps/ui/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      primary: '#FFBC4B',
+      white: '#F4F2F4',
+      black: '#232325',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
