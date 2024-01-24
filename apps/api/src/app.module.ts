badebab25aa58f5dda87a8778af6851d4efe725b
S K B <47430686+shravan20@users.noreply.github.com>
2024-01-24 09:39:42 +0530

    feat(arch): commitlinter | dbconnection | env setup (#15)

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,14 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './modules/user/app.controller';
+import { AppService } from './modules/user/app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: join('.', '.env'),
+        }),
         ServeStaticModule.forRoot({
+            /**
+             * TODO: Make this configurable
+             */
             rootPath: join(__dirname, '../..', 'ui', 'dist'),
         }),
+        DatabaseModule,
     ],
     controllers: [AppController],
     providers: [AppService],
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
+    dotenv.config();
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('/api');
     await app.listen(3000);
-    console.log('Server running on:' + 3000);
+    console.log('Server running on:', process.env.SERVER_PORT || 3000);
 }
 bootstrap();
