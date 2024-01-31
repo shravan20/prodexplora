70ec51abd354e89d0a4e4042b8fa292a6b56b0ba
shravan20 <mrshravankumarb@gmail.com>
2024-01-31 14:54:26 +0530

    feat(model) : include user entity model

--- apps/api/src/entities/user.entity.ts
+++ apps/api/src/entities/user.entity.ts
@@ -2,16 +2,13 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Types } from 'mongoose';
 import { CommonEntity } from './common-entity';
 
-
 interface OAuthProvider {
     provider: string;
     id: string;
 }
 
-
 @Schema({ timestamps: true })
 export class User extends CommonEntity {
-
     @Prop({ required: true, unique: true })
     username: string;
 
