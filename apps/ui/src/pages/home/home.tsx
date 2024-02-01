930b367fe332a8e408d96c1478885c5f33090817
Admin <arnavnath550@gmail.com>
2024-02-01 14:50:57 +1300

    Home page banner added to home.tsx, wrapped around wrapper component

--- apps/ui/src/pages/home/home.tsx
+++ apps/ui/src/pages/home/home.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Navigation from '../../components/navigation';
+import Wrapper from '../../components/wrapper/wrapper';
+import HomeBanner from '../../components/home/home-banner';
 
 const HomePage: React.FC = () => {
     return (
         <div className="gyan-container">
-            <Navigation />
+                <Navigation />
+                <Wrapper>
+                    <div className="flex flex-col gap-5">
+                        <HomeBanner />
+                    </div>
+                </Wrapper>
         </div>
     )
 };
