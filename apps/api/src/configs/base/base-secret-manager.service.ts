0f4cd5da990c940b62187d19847adaed85f78b40
shravan20 <mrshravankumarb@gmail.com>
2024-02-06 02:27:35 +0530

    feat(iam-service):iam service

--- apps/api/src/configs/base/base-secret-manager.service.ts
+++ apps/api/src/configs/base/base-secret-manager.service.ts
@@ -5,11 +5,13 @@ export interface ISecretsManager {
 }
 
 export class BaseSecretManager implements ISecretsManager {
-    constructor(protected readonly configService: ConfigService) { }
+    constructor(protected readonly configService: ConfigService) {}
 
     getSecret<T>(key: string): T | null {
         if (!key) {
-            throw new Error(`Secret key ${key} is required but was not provided.`);
+            throw new Error(
+                `Secret key ${key} is required but was not provided.`,
+            );
         }
         const value = this.configService.get(key);
         if (value) {
