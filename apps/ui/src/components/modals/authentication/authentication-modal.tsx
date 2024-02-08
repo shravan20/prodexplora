b373edcbfeffc0468a05d000eadbd4d012f54acc
shravan20 <mrshravankumarb@gmail.com>
2024-02-08 16:26:33 +0530

    fix(lint): changes

--- apps/ui/src/components/modals/authentication/authentication-modal.tsx
+++ apps/ui/src/components/modals/authentication/authentication-modal.tsx
@@ -1,27 +1,26 @@
-//@ts-nocheck
 import { GoogleLogin } from '@react-oauth/google';
 import * as React from 'react';
 
 const AuthenticationModal: React.FC = () => {
-  return (
-    <div className="p-[25px] bg-base-gradient rounded-md w-[500px]">
+    return (
+        <div className="p-[25px] bg-base-gradient rounded-md w-[500px]">
 
-      <div className="text-center flex flex-col gap-2 load-into-place-anim">
-        <div className="text-2xl text-white font-semibold">Build and Discover the Future</div>
-        <div className="text-m text-white opacity-50 font-medium">Get Started, Login / Sign up</div>
-      </div>
-      <div className="flex flex-col gap-2 m-auto flex items-center justify-center p-5">
-        <GoogleLogin
-          onSuccess={credentialResponse => {
-            console.log(credentialResponse);
-          }}
-          onError={() => {
-            console.log('Login Failed');
-          }}
-        />
-      </div>
-    </div>
-  )
+            <div className="text-center flex flex-col gap-2 load-into-place-anim">
+                <div className="text-2xl text-white font-semibold">Build and Discover the Future</div>
+                <div className="text-m text-white opacity-50 font-medium">Get Started, Login / Sign up</div>
+            </div>
+            <div className="flex flex-col gap-2 m-auto flex items-center justify-center p-5">
+                <GoogleLogin
+                    onSuccess={credentialResponse => {
+                        console.log(credentialResponse);
+                    }}
+                    onError={() => {
+                        console.log('Login Failed');
+                    }}
+                />
+            </div>
+        </div>
+    )
 }
 
 export default AuthenticationModal;
