5fffa09e976347b2700d7418399ef3707f3d32e5
Admin <arnavnath550@gmail.com>
2024-02-08 19:33:40 +1300

    Google client secret moved as an .ENV VARIABLE

--- apps/ui/src/App.tsx
+++ apps/ui/src/App.tsx
@@ -1,14 +1,13 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/home/home';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import { GOOGLE_CLIENT_ID } from './helpers/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
 import { AuthProvider } from './context/AuthContext';
+import HomePage from './pages/home/home';
 
 function App() {
     return (
         <AuthProvider>
-            <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+            <GoogleOAuthProvider clientId={import.meta.env.REACT_APP_GOOGLE_CLIENT_ID}>
                 <BrowserRouter>
                     <Routes>
                         <Route path="/" element={<HomePage />}>
@@ -18,7 +17,6 @@ function App() {
                 </BrowserRouter>
             </GoogleOAuthProvider>
         </AuthProvider>
-            
     );
 }
 
--- apps/ui/src/pages/home/home.tsx
+++ apps/ui/src/pages/home/home.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import Navigation from '../../components/navigation';
-import Wrapper from '../../components/wrapper/wrapper';
 import HomeBanner from '../../components/home/home-banner';
+import Navigation from '../../components/navigation';
 import FilterSidebar from '../../components/view/filters/filter-sidebar';
 import ProjectItem from '../../components/view/projects/project-item';
+import Wrapper from '../../components/wrapper/wrapper';
 
 const HomePage: React.FC = () => {
     return (
         <div className="gyan-container">
-                <Navigation />
-                <Wrapper>
-                    <div className="flex flex-col gap-5">
-                        <HomeBanner />
-                        <div className="flex flex-row gap-2 w-[988px] m-auto">
-                            <FilterSidebar /> 
-                            <div className="w-full flex flex-col gap-2">
-                                <ProjectItem 
-                                    projectIcon="https://avatars.githubusercontent.com/u/79145102?s=500&v=4"
-                                    projectName="Cal.com"
-                                    projectTagline="Scheduling infrastructure for everyone"
-                                    currentCount={55}
-                                />
-                            </div>
+            <Navigation />
+            <Wrapper>
+                <div className="flex flex-col gap-5">
+                    <HomeBanner />
+                    <div className="flex flex-row gap-2 w-[988px] m-auto">
+                        <FilterSidebar />
+                        <div className="w-full flex flex-col gap-2">
+                            <ProjectItem
+                                projectIcon="https://avatars.githubusercontent.com/u/79145102?s=500&v=4"
+                                projectName="Cal.com"
+                                projectTagline="Scheduling infrastructure for everyone"
+                                currentCount={55}
+                            />
                         </div>
                     </div>
-                </Wrapper>
+                </div>
+            </Wrapper>
         </div>
     )
 };
