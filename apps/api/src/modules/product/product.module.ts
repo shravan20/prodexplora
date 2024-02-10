ed464f79c6344c18af7ce5fdf6118be7fb368c7e
shravan20 <mrshravankumarb@gmail.com>
2024-02-10 10:54:15 +0530

    feat(product-api): Set CRUD APIs for Product Flow

--- apps/api/src/modules/product/product.module.ts
+++ apps/api/src/modules/product/product.module.ts
@@ -1,13 +1,19 @@
+import { Product, ProductSchema } from '@entities/product.entity';
 import { Module } from '@nestjs/common';
-import { ProductService } from './product.service';
-import { ProductController } from './product.controller';
-import { ProductUpvoteModule } from './product-upvote/product-upvote.module';
-import { ProductRatingModule } from './product-rating/product-rating.module';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ProductDiscussionModule } from './product-discussion/product-discussion.module';
+import { ProductRatingModule } from './product-rating/product-rating.module';
+import { ProductUpvoteModule } from './product-upvote/product-upvote.module';
+import { ProductController } from './product.controller';
+import { ProductRepository } from './product.repository';
+import { ProductService } from './product.service';
 
 @Module({
+
     controllers: [ProductController],
-    providers: [ProductService],
-    imports: [ProductUpvoteModule, ProductRatingModule, ProductDiscussionModule]
+    providers: [ProductService, ProductRepository],
+    imports: [ProductUpvoteModule, ProductRatingModule, ProductDiscussionModule,
+        MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+    ]
 })
-export class ProductModule {}
+export class ProductModule { }
