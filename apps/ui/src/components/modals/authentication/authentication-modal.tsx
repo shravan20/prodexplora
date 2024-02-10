a42926ab88bf43190864965cce7b2f220d659bec
shravan20 <mrshravankumarb@gmail.com>
2024-02-10 10:37:39 +0530

    fix(auth-modal): Fix auth logic for making google call for information [90m<footer>[39mfix(auth-modal): Fix auth logic for making google call for information

--- apps/ui/src/components/modals/authentication/authentication-modal.tsx
+++ apps/ui/src/components/modals/authentication/authentication-modal.tsx
@@ -5,70 +5,71 @@ import axios from 'axios';
 import { signIn } from '../../../api/services/authentication/authentication.service';
 
 const AuthenticationModal: React.FC = () => {
-  const [user, setUser] = React.useState([]);
-  const [profile, setProfile] = React.useState([]);
+    const [user, setUser] = React.useState([]);
+    const [profile, setProfile] = React.useState([]);
+    const [response, setResponse] = React.useState({});
 
-  const login = useGoogleLogin({
-    onSuccess: (codeResponse: any) => setUser(codeResponse),
-    onError: (error) => console.log('Login Failed:', error)
-  });
+    const login = useGoogleLogin({
+        onSuccess: async (codeResponse: any) => {
 
-  const handleUserSignIn = async (data: any) => {
-    const response = await signIn(data);
-    console.log(`response`, response);
-  }
+            setUser(codeResponse)
+            let accessToken = codeResponse.access_token;
 
-  React.useEffect(
-    async () => {
-      if (user) {
-        const response = await axios
-          .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-            headers: {
-              Authorization: `Bearer ${user.access_token}`,
-              Accept: 'application/json'
+            if (accessToken) {
+                const response: any = await axios
+                    .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${accessToken}`, {
+                        headers: {
+                            Authorization: `Bearer ${accessToken}`,
+                            Accept: 'application/json'
+                        }
+                    });
+
+                const firstName = response['given_name'];
+                const lastName = response['family_name'];
+                const email = response['email'];
+
+                const data = {
+                    "authProvider": [{
+                        "identifier": email,
+                        "provider": "GOOGLE"
+                    }],
+                    "email": email,
+                    "firstName": firstName,
+                    "lastName": lastName
+                }
+                handleUserSignIn(data);
             }
-          });
-        //Run service for login
-        const firstName = response.data.given_name,
-          lastName = response.data.family_name,
-          email = response.data.email;
+        },
+        onError: (error) => console.log('Login Failed:', error)
+    });
 
-        const data = {
-          "authProvider": [{
-            "identifier": email,
-            "provider": "GOOGLE"
-          }],
-          "email": email,
-          "firstName": firstName,
-          "lastName": lastName
-        }
+    const handleUserSignIn = async (data: any) => {
+        const response = await signIn(data);
+        console.log(`response`, response);
+    }
 
-        handleUserSignIn(data);
-      }
-    },
-    [user]
-  );
+    React.useEffect(() => { }, [user]);
 
-  // log out function to log the user out of google and set the profile array to null
-  const logOut = () => {
-    googleLogout();
-    setProfile(null);
-  };
+    // log out function to log the user out of google and set the profile array to null
+    const logOut = () => {
+        googleLogout();
+        setProfile([]);
+    };
 
 
-  return (
-    <div className="p-[25px] bg-base-gradient rounded-md w-[500px]">
+    return (
+        <div className="p-[25px] bg-base-gradient rounded-md w-[500px]">
 
-      <div className="text-center flex flex-col gap-2 load-into-place-anim">
-        <div className="text-2xl text-white font-semibold">Build and Discover the Future</div>
-        <div className="text-m text-white opacity-50 font-medium">Get Started, Login / Sign up</div>
-      </div>
-      <div className="flex flex-col gap-2 m-auto flex items-center justify-center p-5">
+            <div className="text-center flex flex-col gap-2 load-into-place-anim">
+                <div className="text-2xl text-white font-semibold">Build and Discover the Future</div>
+                <div className="text-m text-white opacity-50 font-medium">Get Started, Login / Sign up</div>
+            </div>
+            <div className="flex flex-col gap-2 m-auto flex items-center justify-center p-5">
 
-        <button onClick={() => login()}>Sign in with Google 🚀 </button>
-      </div>
-    </div>
-  )
+                <button onClick={() => login()}>Sign in with Google 🚀 </button>
+            </div>
+        </div>
+    )
 }
 
 export default AuthenticationModal;
