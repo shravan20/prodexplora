70ea122ed7cdf2bf78af99d2d4b11a7133f62614
shravan20 <mrshravankumarb@gmail.com>
2024-02-13 08:59:02 +0530

    feat(product-upvote): repo setup

---
+++ apps/api/src/modules/product/product-upvote/product-upvote.repository.ts
@@ -0,0 +1,12 @@
+import { ProductUpvote, ProductUpvote as ProductUpvoteEntity } from "@entities/product-upvote.entity";
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+
+@Injectable()
+export class ProductUpvoteRepository {
+
+    constructor(@InjectModel(ProductUpvote.name) private readonly model: Model<ProductUpvoteEntity>) {
+    }
+
+}
