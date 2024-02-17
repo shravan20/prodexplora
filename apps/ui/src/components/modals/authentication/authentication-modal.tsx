f4edfb58163b2c2d1dc72a950500c6339158d532
Arnav Nath <130316679+ArnavNath550@users.noreply.github.com>
2024-02-17 22:36:11 +1300

    Feature: Authentication (#38)
    Co-authored-by: shravan20 <mrshravankumarb@gmail.com>
    Co-authored-by: S K B <47430686+shravan20@users.noreply.github.com>

--- apps/ui/src/components/modals/authentication/authentication-modal.tsx
+++ apps/ui/src/components/modals/authentication/authentication-modal.tsx
@@ -1,7 +1,74 @@
-import { GoogleLogin } from '@react-oauth/google';
+import { useGoogleLogin } from '@react-oauth/google';
 import * as React from 'react';
 
+import axios from 'axios';
+import { signIn } from '../../../api/services/authentication/authentication.service';
+import { useAuth } from '../../../context/AuthContext';
+
+import { FaGithub, FaGoogle } from 'react-icons/fa';
+import { Button } from '../../../packages/ui/components/buttons/Button';
+
 const AuthenticationModal: React.FC = () => {
+    const [user, setUser] = React.useState([]);
+    const [profile, setProfile] = React.useState([]);
+    const [response, setResponse] = React.useState({});
+
+    const { authLogin } = useAuth();
+
+    const login = useGoogleLogin({
+        onSuccess: async (codeResponse: any) => {
+
+            setUser(codeResponse)
+            let accessToken = codeResponse.access_token;
+
+            if (accessToken) {
+                const response: any = await axios
+                    .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${accessToken}`, {
+                        headers: {
+                            Authorization: `Bearer ${accessToken}`,
+                            Accept: 'application/json'
+                        }
+                    });
+
+                const firstName = response['given_name'];
+                const lastName = response['family_name'];
+                const email = response['email'];
+
+                const data = {
+                    "authProvider": [{
+                        "identifier": email,
+                        "provider": "GOOGLE"
+                    }],
+                    "email": email,
+                    "firstName": firstName,
+                    "lastName": lastName
+                }
+                console.log(`data`, data);
+                // handleUserSignIn(data);
+            }
+        },
+        onError: (error) => console.log('Login Failed:', error)
+    });
+
+    const handleUserSignIn = async (data: any) => {
+        const response = await signIn(data);
+        authLogin(response.data.user._id, response.data.access_token, response.data.user.email);
+        window.location.reload();
+    }
+
+    const loginWithGithub = async () => {
+        window.location.href = "https://github.com/login/oauth/authorize?client_id=" + import.meta.env.VITE_GITHUB_CLIENT_ID + "&scope=read:user, user:email, user:follow, read:project, repo:public_repo" + "&redirect_uri=localhost:5173";
+    }
+
+    React.useEffect(() => {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const codeParams = urlParams.get('code');
+        console.log("====>>>>>>" + codeParams);
+    }, []);
+
+
+
     return (
         <div className="p-[25px] bg-base-gradient rounded-md w-[500px]">
 
@@ -10,14 +77,19 @@ const AuthenticationModal: React.FC = () => {
                 <div className="text-m text-white opacity-50 font-medium">Get Started, Login / Sign up</div>
             </div>
             <div className="flex flex-col gap-2 m-auto flex items-center justify-center p-5">
-                <GoogleLogin
-                    onSuccess={credentialResponse => {
-                        console.log(credentialResponse);
-                    }}
-                    onError={() => {
-                        console.log('Login Failed');
-                    }}
-                />
+
+                <Button onClick={() => login()} variant="button" color="fun">
+                    <div className="flex flex-row gap-2 text-m font-semibold items-center">
+                        <FaGoogle />
+                        Sign in with Google
+                    </div>
+                </Button>
+                <Button onClick={() => loginWithGithub()} variant="button" color="subtle">
+                    <div className="flex flex-row gap-2 text-m font-semibold items-center">
+                        <FaGithub />
+                        Sign in with Github
+                    </div>
+                </Button>
             </div>
         </div>
     )
--- apps/ui/src/context/AuthContext.tsx
+++ apps/ui/src/context/AuthContext.tsx
@@ -1,68 +1,76 @@
 "use client"
 import React, { ReactNode, createContext, useContext, useState } from 'react';
+import { getDataMethod } from '../api/api';
+import { API_HOST } from '../api/api-host';
 
 interface AuthContextType {
-    token: string | null;
-    email: string | null;
-    authLogin: (newToken: string, accessToken: string, email: string) => void;
-    authSignup: (newToken: string) => void;
-    isAuthenticated: () => void;
-    authLogout: () => void;
+  token: string | null;
+  email: string | null;
+  authLogin: (newToken: string, accessToken: string, email: string) => void;
+  authSignup: (newToken: string) => void;
+  fetchUserDetails: (userToken: string) => void;
+  isAuthenticated: () => void;
+  authLogout: () => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 interface AuthProviderProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-    const [token, setToken] = useState<string | null>(
-        typeof window !== 'undefined' ? localStorage.getItem('userToken') || null : null
-    );
+  const [token, setToken] = useState<string | null>(
+    typeof window !== 'undefined' ? localStorage.getItem('userToken') || null : null
+  );
 
-    const [email, setEmail] = useState<string | null>(
-        typeof window !== 'undefined' ? localStorage.getItem('email') || null : null
-    );
+  const [email, setEmail] = useState<string | null>(
+    typeof window !== 'undefined' ? localStorage.getItem('email') || null : null
+  );
 
-    const authLogin = (newToken: string, accessToken: string, email: string) => {
-        setToken(newToken);
-        localStorage.setItem('userToken', newToken);
-        localStorage.setItem('accessToken', accessToken);
-        localStorage.setItem('email', email);
-    };
+  const authLogin = (newToken: string, accessToken: string, email: string) => {
+    setToken(newToken);
+    localStorage.setItem('userToken', newToken);
+    localStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('email', email);
+  };
 
-    const authLogout = () => {
-        setToken(null);
-        setEmail(null);
-        localStorage.removeItem('userToken');
-        localStorage.removeItem('email');
-    };
+  const authLogout = () => {
+    setToken(null);
+    setEmail(null);
+    localStorage.removeItem('userToken');
+    localStorage.removeItem('email');
+  };
 
-    const authSignup = (newToken: string) => {
-        setToken(newToken);
-        localStorage.setItem('userToken', newToken);
+  const fetchUserDetails = (userToken: string) => {
+    const response = getDataMethod(API_HOST + "/users/" + userToken);
+    return response;
+  }
+
+  const authSignup = (newToken: string) => {
+    setToken(newToken);
+    localStorage.setItem('userToken', newToken);
+  }
+
+  const isAuthenticated = () => {
+    if (token !== null || token !== "") {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    const isAuthenticated = () => {
-        if (token !== null || token !== "") {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    return (
-        <AuthContext.Provider value={{ token, email, authLogin, authSignup, authLogout, isAuthenticated }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ token, email, fetchUserDetails, authLogin, authSignup, authLogout, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = (): AuthContextType => {
-    const context = useContext(AuthContext);
-    if (!context) {
-        throw new Error('useAuth must be used within an AuthProvider');
-    }
-    return context;
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
 };
