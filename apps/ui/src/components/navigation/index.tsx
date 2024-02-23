c64ca7d11b629a6f58b823d6cf263a5fe60b0465
ArnavNath550 <arnavnath550@gmail.com>
2024-02-23 13:24:29 +1300

    simple tags should be closed

--- apps/ui/src/components/navigation/index.tsx
+++ apps/ui/src/components/navigation/index.tsx
@@ -2,14 +2,13 @@ import * as React from 'react';
 import { SearchInput } from '../../packages/ui/components/forms/inputs/SearchInput';
 
 import { GiArrowCursor } from 'react-icons/gi';
-import { GoArrowLeft, GoIssueDraft, GoPersonFill, GoProjectTemplate, GoStack, GoStarFill } from "react-icons/go";
+import { GoIssueDraft, GoProjectTemplate, GoStack, GoStarFill } from "react-icons/go";
 import { useAuth } from '../../context/AuthContext';
 import { Button } from '../../packages/ui/components/buttons/Button';
 import AnimatedDialog from '../../packages/ui/components/dialog/AnimatedDialog';
 import AuthenticationModal from '../modals/authentication/authentication-modal';
 import CreateProjectModal from '../modals/project/create-project-modal';
 
-import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 
 const Navigation: React.FC = () => {
     const { token, fetchUserDetails, authLogout } = useAuth();
@@ -71,39 +70,18 @@ const Navigation: React.FC = () => {
                         }
                         dialogContent={<CreateProjectModal />}
                     />
-                    <AnimatedDialog
-                        dialogTrigger={
-                            <Button color="subtle" size="lg">
-                                <div className="flex flex-row gap-1 items-center">
-                                    <GoStarFill />
-                                    <span>Login</span>
-                                </div>
-                            </DropdownMenu.Trigger>
-
-                            <DropdownMenu.Portal>
-                                <DropdownMenu.Content
-                                    className="min-w-[220px] bg-hero-gradient bg-primary-lighter rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade z-50 pb-3.5"
-                                    sideOffset={5}
-                                >
-                                    <div className="text-xl font-semibold p-3.5 pt-2.5 leading-tight">
-                                        Hey, <br />
-                                        {userData['firstName']} {userData['lastName']}
+                    {token ? (
+                        <AnimatedDialog
+                            dialogTrigger={
+                                <Button color="subtle" size="lg">
+                                    <div className="flex flex-row gap-1 items-center">
+                                        <GoStarFill />
+                                        <span>Login</span>
                                     </div>
-                                    <DropdownMenu.Item className="group hover:bg-primary transition-all text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] p-3.5 relative select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1 cursor-pointer">
-                                        <div className="flex flex-row gap-2 items-center-justify-center">
-                                            <GoPersonFill />
-                                            <span className="error">Profile</span>
-                                        </div>
-                                    </DropdownMenu.Item>
-                                    <DropdownMenu.Item className="group text-[13px] hover:bg-primary transition-all leading-none text-violet11 rounded-[3px] flex items-center h-[25px] relative p-3.5 select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1 cursor-pointer" onClick={() => handleLogout()}>
-                                        <div className="flex flex-row gap-2 items-center-justify-center">
-                                            <GoArrowLeft />
-                                            <span className="error">Logout</span>
-                                        </div>
-                                    </DropdownMenu.Item>
-                                </DropdownMenu.Content>
-                            </DropdownMenu.Portal>
-                        </DropdownMenu.Root>
+                                </Button>
+                            }
+                            dialogContent={<AuthenticationModal />}
+                        />
                     ) : (
                         <AnimatedDialog
                             dialogTrigger={
