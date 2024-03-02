9c4fc0105ad2e6cd06e5b37da98a417d13ab578a
ArnavNath550 <arnavnath550@gmail.com>
2024-03-02 14:55:26 +1300

    last line added

--- apps/ui/src/App.tsx
+++ apps/ui/src/App.tsx
@@ -1,22 +1,13 @@
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import ProjectRouter from './components/projectRouter';
 import { AuthProvider } from './context/AuthContext';
-import HomePage from './pages/home/home';
-import Product from './pages/product/product';
 
 function App() {
     return (
         <AuthProvider>
             <GoogleOAuthProvider clientId={import.meta.env.VITE_APP_GOOGLE_CLIENT_ID}>
-                <BrowserRouter>
-                    <Routes>
-                        <Route>
-                            <Route path="/" element={<HomePage />} />
-                            <Route path="product/:id" element={<Product />} />
-                        </Route>
-                    </Routes>
-                </BrowserRouter>
+                <ProjectRouter />
             </GoogleOAuthProvider>
         </AuthProvider>
     );
