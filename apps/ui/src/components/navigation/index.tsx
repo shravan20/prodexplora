552486e202320def2817bdf9b07f8e1c546a2a27
shravan20 <mrshravankumarb@gmail.com>
2024-03-06 23:14:04 +0530

    fix: merge conflicts

--- apps/ui/src/components/navigation/index.tsx
+++ apps/ui/src/components/navigation/index.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import { SearchInput } from '../../packages/ui/components/forms/inputs/SearchInput';
 import { GiArrowCursor, GiHamburgerMenu } from 'react-icons/gi';
 import { GoArrowLeft, GoIssueDraft, GoPersonFill, GoProjectTemplate, GoSearch, GoStack, GoStarFill, GoX } from "react-icons/go";
 import { useAuth } from '../../context/AuthContext';
 import { Button } from '../../packages/ui/components/buttons/Button';
 import AnimatedDialog from '../../packages/ui/components/dialog/AnimatedDialog';
+import { SearchInput } from '../../packages/ui/components/forms/inputs/SearchInput';
 import AuthenticationModal from '../modals/authentication/authentication-modal';
 import CreateProjectModal from '../modals/project/create-project-modal';
 
@@ -81,33 +81,33 @@ const Navigation: React.FC = () => {
                                 </Button>
                             </DropdownMenu.Trigger>
 
-                            <DropdownMenu.Portal>
-                                <DropdownMenu.Content
-                                    className="min-w-[220px] bg-hero-gradient bg-primary-lighter rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade z-50 pb-3.5"
-                                    sideOffset={5}
-                                >
-                                    <div className="text-xl font-semibold p-3.5 pt-2.5 leading-tight">
-                                        Hey
-                                    </div>
-                                </Button>
+                <DropdownMenu.Portal>
+                    <DropdownMenu.Content
+                        className="min-w-[220px] bg-hero-gradient bg-primary-lighter rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade z-50 pb-3.5"
+                        sideOffset={5}
+                    >
+                        <div className="text-xl font-semibold p-3.5 pt-2.5 leading-tight">
+                            Hey
+                        </div>
+                    </Button>
                             }
-                            dialogContent={<AuthenticationModal />}
+                    dialogContent={<AuthenticationModal />}
                         />
                     ) : (
-                        <AnimatedDialog
-                            dialogTrigger={
-                                <Button color="subtle" size="lg">
-                                    <div className="flex flex-row gap-1 items-center">
-                                        <GoStarFill />
-                                        <span>Login</span>
-                                    </div>
-                                </Button>
-                            }
-                            dialogContent={<AuthenticationModal />}
-                        />
+                    <AnimatedDialog
+                        dialogTrigger={
+                            <Button color="subtle" size="lg">
+                                <div className="flex flex-row gap-1 items-center">
+                                    <GoStarFill />
+                                    <span>Login</span>
+                                </div>
+                            </Button>
+                        }
+                        dialogContent={<AuthenticationModal />}
+                    />
                     )}
-                </div>
             </div>
+        </div >
             <div className="h-50 w-full bg-primary bg-hero-gradient border-b-primary-lighter border-b-[1px] fixed top-0 p-4 pt-2 pb-2 backdrop-blur-sm z-30">
                 <div className="mobile-only">
                     <div className="flex flex-row gap-5 items-center justify-between">
