3c477e7b62dd90473b1b235c808304bac3eb9453
ArnavNath550 <arnavnath550@gmail.com>
2024-03-08 14:34:44 +1300

    user-profile responsive completed, dynamic component data added too

--- apps/ui/src/pages/user-profile/user-profile.tsx
+++ apps/ui/src/pages/user-profile/user-profile.tsx
@@ -7,9 +7,9 @@ import { Button } from '../../packages/ui/components/buttons/Button';
 const UserProfile: React.FC = () => {
     const [userData, setUserData] = React.useState([
         {
-            'profilePic': 'https://ph-files.imgix.net/39eadfe0-8f39-40f8-8213-ee9e516df919.png?auto=compress&codec=mozjpeg&cs=strip&auto=format&w=64&h=64&fit=crop&dpr=2',
-            'fullName': 'Cal.com',
-            'headline': 'Issue tracking that feels like progress',
+            'profilePic': 'https://i.pinimg.com/736x/f5/c3/39/f5c339edc983ba515ada22ff28c957ba.jpg',
+            'fullName': 'Muna Das',
+            'headline': 'I love making open-source projects for devs 💻🖱',
             'usersFollowing': 0,
             'usersFollowers': 0,
             'about': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -19,14 +19,29 @@ const UserProfile: React.FC = () => {
     const [userLoading, setUserLoading] = React.useState(false);
     const [productDataLoading, setProductDataLoading] = React.useState(true);
 
-    const [data, setData] = React.useState([]);
+    const [data, setData] = React.useState([
+        {
+            'projectId': 25,
+            'projectIcon': 'https://ph-files.imgix.net/39eadfe0-8f39-40f8-8213-ee9e516df919.png?auto=compress&codec=mozjpeg&cs=strip&auto=format&w=64&h=64&fit=crop&dpr=2',
+            'projectName': 'Cal.com',
+            'projectTagline': 'Scheduling infrastructure for everyone',
+            'currentCount': 55
+        },
+        {
+            'projectId': 20,
+            'projectIcon': 'https://ph-files.imgix.net/3bb3ea09-8afe-42b0-8545-160370510ec5.jpeg?auto=compress&codec=mozjpeg&cs=strip&auto=format&w=64&h=64&fit=crop&dpr=2',
+            'projectName': 'Dub',
+            'projectTagline': 'Short Links with Superpowers',
+            'currentCount': 35
+        },
+    ]);
 
     return (
         <div className="bg-primary w-full h-full">
             <Navigation />
             <Wrapper>
                 {userLoading == false ? (
-                    <div className="flex flex-row justify-between w-[650px] m-auto pt-5 pb-5 items-center">
+                    <div className="flex flex-row justify-between max-w-[650px] m-auto pt-5 pb-5 items-center">
                     <div className="flex flex-row gap-8 items-center">
                         <div className="w-[90px] h-[90px] bg-white rounded-full">
                             <img src={userData[0].profilePic}  className="w-[90px] h-[90px] rounded-full" />
@@ -65,18 +80,16 @@ const UserProfile: React.FC = () => {
                     <></>
                 )}
                 <div className="max-w-[650px] m-auto flex flex-col gap-2 pt-5 pb-5">
-                    <ProjectItem
-                        projectIcon="https://avatars.githubusercontent.com/u/79145102?s=500&v=4"
-                        projectName="Cal.com"
-                        projectTagline="Scheduling infrastructure for everyone"
-                        currentCount={55}
-                    />
-                    <ProjectItem
-                        projectIcon="https://avatars.githubusercontent.com/u/79145102?s=500&v=4"
-                        projectName="Cal.com"
-                        projectTagline="Scheduling infrastructure for everyone"
-                        currentCount={55}
-                    />
+                    {data.map((y) => {
+                        return (
+                            <ProjectItem
+                                projectIcon={y.projectIcon}
+                                projectName={y.projectName}
+                                projectTagline={y.projectTagline}
+                                currentCount={y.currentCount}
+                            />
+                        )
+                    })}
                 </div>
             </Wrapper>
         </div>
