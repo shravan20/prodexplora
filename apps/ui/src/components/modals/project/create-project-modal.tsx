6409246b82b8603ba34c7390f095f8b3c0dbd43e
shravan20 <mrshravankumarb@gmail.com>
2024-03-11 02:49:53 +0530

    fix: integration issues

--- apps/ui/src/components/modals/project/create-project-modal.tsx
+++ apps/ui/src/components/modals/project/create-project-modal.tsx
@@ -4,41 +4,74 @@ import SelectRepoStage from './stages/select-repo-stage';
 import SocialLinksStage from './stages/social-links-stage';
 
 const CreateProjectModal: React.FC = () => {
+
+    type RepositoryData = {
+        title: string,
+        description: string,
+        slug: string,
+        technologies: [
+            string
+        ],
+    }
+
     const [selectedStage, setSelectedStage] = React.useState<number>(0);
-    const [stage, setStage] = React.useState([<SelectRepoStage setStage={setSelectedStage} />, <ProjectDetailsStage setStage={setSelectedStage} />, <SocialLinksStage setStage={setSelectedStage} />])
+
+    /**
+     * Handle repository selection
+     */
+    const [repository, setRepository] = React.useState<RepositoryData | null>(null);
+    const handleSelectRepo = (selectedRepository: RepositoryData) => {
+        setRepository(selectedRepository);
+        setSelectedStage(selectedStage + 1);
+    };
+
+
+    const stages = [
+        {
+            title: "Submit Product",
+            description: "Let's change the world with your amazing project",
+            component: <SelectRepoStage onSelectRepo={handleSelectRepo} />
+        },
+        {
+            title: "Submit Product",
+            description: "Let's change the world with your amazing project",
+            component: <ProjectDetailsStage />
+        },
+        {
+            title: "Product / Links",
+            description: "Add all the places that link to your product",
+            component: <SocialLinksStage />
+        }
+    ];
+
+
     return (
         <div className="p-5 pl-8 pr-8 w-[550px] h-full bg-base-gradient rounded-md">
-            {selectedStage == 0 || selectedStage == 1 ? (
+            {(selectedStage === 0 || selectedStage === 1) && (
                 <div className="flex flex-col gap-1 mb-5">
                     <div className="text-xl font-semibold text-white">
-                        Submit Product
+                        {stages[selectedStage].title}
                     </div>
                     <div className="text-s font-regular text-info">
-                        Let's change the world with your amazing project
+                        {stages[selectedStage].description}
                     </div>
                 </div>
-            ) : (
-                <></>
             )}
 
-            {selectedStage == 2 ? (
+            {selectedStage === 2 && (
                 <div className="flex flex-col gap-1 mb-5">
                     <div className="text-xl font-semibold text-white">
-                        Product / Links
+                        {stages[selectedStage].title}
                     </div>
                     <div className="text-s font-regular text-info">
-                        Add all the places that link to your product
+                        {stages[selectedStage].description}
                     </div>
                 </div>
-            ) : (
-                <></>
             )}
 
-            <div>
-                {stage[selectedStage]}
-            </div>
+            <div>{stages[selectedStage].component}</div>
         </div>
-    )
-}
+    );
+};
 
 export default CreateProjectModal;
